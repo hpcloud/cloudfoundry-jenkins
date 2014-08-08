237b0511112b1bbfb5ab70ea8c92191f24731842
WilliamG <williamg@activestate.com>
2014-08-08 16:05:57 -0700

    Working prototype that pushes a hello-java app

--- src/main/java/org/activestate/stackatojenkins/StackatoPushBuilder.java
+++ src/main/java/org/activestate/stackatojenkins/StackatoPushBuilder.java
@@ -1,6 +1,7 @@
 package org.activestate.stackatojenkins;
 
 import hudson.Extension;
+import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
@@ -12,8 +13,8 @@ import org.cloudfoundry.client.lib.CloudFoundryClient;
 import org.cloudfoundry.client.lib.domain.Staging;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,12 +43,50 @@ public class StackatoPushBuilder extends Builder {
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
 
         try {
-            URL targetUrl = new URL(target);
+            String appName = build.getProject().getDisplayName();
+
+            String fullTarget = target;
+            if (!fullTarget.startsWith("https://")) {
+                if (!fullTarget.startsWith("api.")) {
+                    fullTarget = "https://api." + fullTarget;
+                } else {
+                    fullTarget = "https://" + fullTarget;
+                }
+            }
+            URL targetUrl = new URL(fullTarget);
+
+            listener.getLogger().println("Logging to stackato with:" + username + "/" + password);
+            listener.getLogger().println("Target URL:" + targetUrl.getHost());
+            listener.getLogger().println("Org:" + organization);
+            listener.getLogger().println("Space:" + cloudSpace);
+
             CloudCredentials credentials = new CloudCredentials(username, password);
             CloudFoundryClient client = new CloudFoundryClient(credentials, targetUrl, organization, cloudSpace);
+            client.login();
+
+            listener.getLogger().println("Pushing " + appName + " app to " + fullTarget);
+
+            listener.getLogger().println("Creating new app.");
+            Staging staging = new Staging();
+            List<String> uris = new ArrayList<String>();
+            uris.add(uri);
+            List<String> services = new ArrayList<String>();
+            client.createApplication(appName, staging, 512, uris, services);
+
+            listener.getLogger().println("Pushing app bits.");
+            FilePath warPath = new FilePath(build.getWorkspace(), "target/hello-java-1.0.war");
+            File warFile = new File(warPath.toURI());
+            client.uploadApplication(appName, warFile);
+
+            //TODO: start app and show url
+
+            listener.getLogger().println("Stackato push successful.");
             return true;
         } catch (MalformedURLException e) {
             listener.getLogger().println("The target URL is not valid: " + e.getMessage());
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace(listener.getLogger());
             return false;
         }
     }
